c6911fe640fea055539f9a0eb8d6b76ea4c7885d
hernja <hernja666@gmail.com>
2018-01-18 17:58:08 -0600

    modificacion

--- src/Registro/Registros.java
+++ src/Registro/Registros.java
@@ -18,6 +18,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.ImageIcon;
 
 public class Registros extends JFrame {
 
@@ -49,7 +52,7 @@ public class Registros extends JFrame {
 	public Registros() {
 		
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		setBounds(100, 100, 450, 477);
+		setBounds(100, 100, 850, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -68,8 +71,8 @@ public class Registros extends JFrame {
 					Class.forName("com.mysql.jdbc.Driver");
 					String url = "jdbc:mysql://localhost/restaurante";
 					String usr = "root";
-					String psw = "RARO97";
-					//String psw = "";//
+					//String psw = "RARO97";
+					String psw = "";//
 
 					
 					con = DriverManager.getConnection(url, usr, psw);
@@ -106,27 +109,37 @@ public class Registros extends JFrame {
 					}
 			}
 		}); 
-		btnInsertar.setBounds(10, 256, 89, 23);
+		btnInsertar.setBounds(10, 380, 89, 23);
 		contentPane.add(btnInsertar);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 11, 414, 234);
+		scrollPane.setBounds(60, 55, 709, 314);
 		contentPane.add(scrollPane);
 		
 		table = new JTable();
 		scrollPane.setViewportView(table);
 		
 		JButton btnModificar = new JButton("modificar");
-		btnModificar.setBounds(120, 256, 89, 23);
+		btnModificar.setBounds(144, 380, 89, 23);
 		contentPane.add(btnModificar);
 		
 		JButton btnActu = new JButton("actualizar");
-		btnActu.setBounds(227, 256, 89, 23);
+		btnActu.setBounds(266, 380, 89, 23);
 		contentPane.add(btnActu);
 		
 		JButton btnEliminar = new JButton("eliminar");
-		btnEliminar.setBounds(335, 256, 89, 23);
+		btnEliminar.setBounds(395, 380, 89, 23);
 		contentPane.add(btnEliminar);
+		
+		JLabel lblNewLabel = new JLabel("REGISTRO DE SU ORDEN");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(310, 11, 152, 23);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setIcon(new ImageIcon(Registros.class.getResource("/ima1/rojo.jpg")));
+		lblNewLabel_1.setBounds(0, 0, 834, 561);
+		contentPane.add(lblNewLabel_1);
 		
 	
 		
