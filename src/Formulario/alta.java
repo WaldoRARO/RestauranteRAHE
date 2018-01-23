519b8c262e10c6a6aa31a4f72ff45842dcc4f9ff
hernja <hernja666@gmail.com>
2018-01-22 20:35:59 -0600

    modificacion

--- src/Formulario/alta.java
+++ src/Formulario/alta.java
@@ -31,6 +31,10 @@ public class alta extends JFrame {
 	private JTextField textField_4;
 	private JTextField textField_5;
 	private JTextField textField_6;
+	private JTextField textField_7;
+	private JTextField textField_8;
+	private JTextField textField_9;
+	private JTextField textField_10;
 
 	/**
 	 * Launch the application.
@@ -71,7 +75,7 @@ public class alta extends JFrame {
 		
 		textField_2 = new JTextField();
 		textField_2.setBounds(20, 243, 338, 32);
-		contentPane.add(textField_2);
+		contentPane.add(textField_2);//ensaladas
 		textField_2.setColumns(10);
 		
 		JButton btnNewButton = new JButton("REGISTRAR");
@@ -104,12 +108,59 @@ public class alta extends JFrame {
 		contentPane.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("REGISTRAR");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Connection con = null;
+				try {	
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://localhost/restaurante";
+					String usr = "root";
+					//String psw = "RARO97";
+					String psw = "";
+					con = DriverManager.getConnection(url, usr, psw);
+					String usuario= textField_2.getText();
+					String query = "INSERT INTO ensaladas (platillo) values ('"+usuario+"')";
+					Statement stm = con.createStatement();
+					stm.executeUpdate(query);
+					textField.setText("");
+					JOptionPane.showMessageDialog(null, "USUARIO INGRESADO A LA BD");
+				} catch (ClassNotFoundException e1) {
+					System.out.println("Error");
+				} catch (SQLException e1) {
+					System.out.println("Error con la  conexión de BD");
+				}	
+			}
+			
+		});
 		btnNewButton_1.setFont(new Font("Arial Black", Font.BOLD | Font.ITALIC, 14));
 		btnNewButton_1.setBackground(Color.ORANGE);
 		btnNewButton_1.setBounds(380, 173, 137, 32);
 		contentPane.add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("REGISTRAR");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Connection con = null;
+				try {	
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://localhost/restaurante";
+					String usr = "root";
+					//String psw = "RARO97";
+					String psw = "";
+					con = DriverManager.getConnection(url, usr, psw);
+					String usuario= textField_1.getText();
+					String query = "INSERT INTO entrada (platillo) values ('"+usuario+"')";
+					Statement stm = con.createStatement();
+					stm.executeUpdate(query);
+					textField.setText("");
+					JOptionPane.showMessageDialog(null, "USUARIO INGRESADO A LA BD");
+				} catch (ClassNotFoundException e1) {
+					System.out.println("Error");
+				} catch (SQLException e1) {
+					System.out.println("Error con la  conexión de BD");
+				}
+			}
+		});
 		btnNewButton_2.setFont(new Font("Arial Black", Font.BOLD | Font.ITALIC, 14));
 		btnNewButton_2.setBackground(Color.ORANGE);
 		btnNewButton_2.setBounds(380, 241, 138, 32);
@@ -160,10 +211,33 @@ public class alta extends JFrame {
 		
 		textField_3 = new JTextField();
 		textField_3.setBounds(20, 310, 338, 32);
-		contentPane.add(textField_3);
+		contentPane.add(textField_3);//mariscos
 		textField_3.setColumns(10);
 		
 		JButton btnNewButton_4 = new JButton("REGISTRAR");
+		btnNewButton_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Connection con = null;
+				try {	
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://localhost/restaurante";
+					String usr = "root";
+					//String psw = "RARO97";
+					String psw = "";
+					con = DriverManager.getConnection(url, usr, psw);
+					String usuario= textField_3.getText();
+					String query = "INSERT INTO mariscos (platillo) values ('"+usuario+"')";
+					Statement stm = con.createStatement();
+					stm.executeUpdate(query);
+					textField.setText("");
+					JOptionPane.showMessageDialog(null, "USUARIO INGRESADO A LA BD");
+				} catch (ClassNotFoundException e1) {
+					System.out.println("Error");
+				} catch (SQLException e1) {
+					System.out.println("Error con la  conexión de BD");
+				}
+			}
+		});
 		btnNewButton_4.setBackground(Color.ORANGE);
 		btnNewButton_4.setForeground(Color.BLACK);
 		btnNewButton_4.setFont(new Font("Arial Black", Font.ITALIC, 14));
@@ -186,18 +260,87 @@ public class alta extends JFrame {
 		textField_6.setColumns(10);
 		
 		JButton btnNewButton_5 = new JButton("REGISTRO");
+		btnNewButton_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Connection con = null;
+				try {	
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://localhost/restaurante";
+					String usr = "root";
+					//String psw = "RARO97";
+					String psw = "";
+					con = DriverManager.getConnection(url, usr, psw);
+					String usuario= textField_4.getText();
+					String query = "INSERT INTO helados (postre) values ('"+usuario+"')";
+					Statement stm = con.createStatement();
+					stm.executeUpdate(query);
+					textField.setText("");
+					JOptionPane.showMessageDialog(null, "USUARIO INGRESADO A LA BD");
+				} catch (ClassNotFoundException e1) {
+					System.out.println("Error");
+				} catch (SQLException e1) {
+					System.out.println("Error con la  conexión de BD");
+				}
+			}
+		});
 		btnNewButton_5.setFont(new Font("Arial Black", Font.ITALIC, 14));
 		btnNewButton_5.setBackground(Color.ORANGE);
 		btnNewButton_5.setBounds(993, 126, 136, 30);
 		contentPane.add(btnNewButton_5);
 		
 		JButton btnNewButton_6 = new JButton("REGISTRO");
+		btnNewButton_6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Connection con = null;
+				try {	
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://localhost/restaurante";
+					String usr = "root";
+					//String psw = "RARO97";
+					String psw = "";
+					con = DriverManager.getConnection(url, usr, psw);
+					String usuario= textField_5.getText();
+					String query = "INSERT INTO tartas (postre) values ('"+usuario+"')";
+					Statement stm = con.createStatement();
+					stm.executeUpdate(query);
+					textField.setText("");
+					JOptionPane.showMessageDialog(null, "USUARIO INGRESADO A LA BD");
+				} catch (ClassNotFoundException e1) {
+					System.out.println("Error");
+				} catch (SQLException e1) {
+					System.out.println("Error con la  conexión de BD");
+				}
+			}
+		});
 		btnNewButton_6.setFont(new Font("Arial Black", Font.ITALIC, 14));
 		btnNewButton_6.setBackground(Color.ORANGE);
 		btnNewButton_6.setBounds(994, 205, 135, 34);
 		contentPane.add(btnNewButton_6);
 		
 		JButton btnNewButton_7 = new JButton("REGISTRAR");
+		btnNewButton_7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Connection con = null;
+				try {	
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://localhost/restaurante";
+					String usr = "root";
+					//String psw = "RARO97";
+					String psw = "";
+					con = DriverManager.getConnection(url, usr, psw);
+					String usuario= textField_6.getText();
+					String query = "INSERT INTO especialidades (postre) values ('"+usuario+"')";
+					Statement stm = con.createStatement();
+					stm.executeUpdate(query);
+					textField.setText("");
+					JOptionPane.showMessageDialog(null, "USUARIO INGRESADO A LA BD");
+				} catch (ClassNotFoundException e1) {
+					System.out.println("Error");
+				} catch (SQLException e1) {
+					System.out.println("Error con la  conexión de BD");
+				}
+			}
+		});
 		btnNewButton_7.setFont(new Font("Arial Black", Font.ITALIC, 14));
 		btnNewButton_7.setBackground(Color.ORANGE);
 		btnNewButton_7.setBounds(993, 285, 136, 32);
@@ -220,5 +363,170 @@ public class alta extends JFrame {
 		lblEspecialidades.setForeground(Color.ORANGE);
 		lblEspecialidades.setBounds(645, 263, 137, 14);
 		contentPane.add(lblEspecialidades);
+		
+		textField_7 = new JTextField();
+		textField_7.setBounds(20, 405, 338, 32);
+		contentPane.add(textField_7);
+		textField_7.setColumns(10);
+		
+		textField_8 = new JTextField();
+		textField_8.setBounds(20, 487, 338, 32);
+		contentPane.add(textField_8);
+		textField_8.setColumns(10);
+		
+		JButton btnNewButton_8 = new JButton("REGISTRAR");
+		btnNewButton_8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Connection con = null;
+				try {	
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://localhost/restaurante";
+					String usr = "root";
+					//String psw = "RARO97";
+					String psw = "";
+					con = DriverManager.getConnection(url, usr, psw);
+					String usuario= textField_7.getText();
+					String query = "INSERT INTO refresco (bebida) values ('"+usuario+"')";
+					Statement stm = con.createStatement();
+					stm.executeUpdate(query);
+					textField.setText("");
+					JOptionPane.showMessageDialog(null, "USUARIO INGRESADO A LA BD");
+				} catch (ClassNotFoundException e1) {
+					System.out.println("Error");
+				} catch (SQLException e1) {
+					System.out.println("Error con la  conexión de BD");
+				}
+			}
+		});
+		btnNewButton_8.setFont(new Font("Arial Black", Font.BOLD | Font.ITALIC, 14));
+		btnNewButton_8.setBackground(Color.ORANGE);
+		btnNewButton_8.setBounds(380, 405, 137, 32);
+		contentPane.add(btnNewButton_8);
+		
+		JButton btnNewButton_9 = new JButton("REGISTRAR");
+		btnNewButton_9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Connection con = null;
+				try {	
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://localhost/restaurante";
+					String usr = "root";
+					//String psw = "RARO97";
+					String psw = "";
+					con = DriverManager.getConnection(url, usr, psw);
+					String usuario= textField_8.getText();
+					String query = "INSERT INTO bebida_alcoholica (bebida) values ('"+usuario+"')";
+					Statement stm = con.createStatement();
+					stm.executeUpdate(query);
+					textField.setText("");
+					JOptionPane.showMessageDialog(null, "USUARIO INGRESADO A LA BD");
+				} catch (ClassNotFoundException e1) {
+					System.out.println("Error");
+				} catch (SQLException e1) {
+					System.out.println("Error con la  conexión de BD");
+				}
+			}
+		});
+		btnNewButton_9.setFont(new Font("Arial Black", Font.BOLD | Font.ITALIC, 14));
+		btnNewButton_9.setBackground(Color.ORANGE);
+		btnNewButton_9.setBounds(380, 487, 137, 32);
+		contentPane.add(btnNewButton_9);
+		
+		JButton btnNewButton_10 = new JButton("REGISTRAR");
+		btnNewButton_10.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Connection con = null;
+				try {	
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://localhost/restaurante";
+					String usr = "root";
+					//String psw = "RARO97";
+					String psw = "";
+					con = DriverManager.getConnection(url, usr, psw);
+					String usuario= textField_9.getText();
+					String query = "INSERT INTO aguas (bebida) values ('"+usuario+"')";
+					Statement stm = con.createStatement();
+					stm.executeUpdate(query);
+					textField.setText("");
+					JOptionPane.showMessageDialog(null, "USUARIO INGRESADO A LA BD");
+				} catch (ClassNotFoundException e1) {
+					System.out.println("Error");
+				} catch (SQLException e1) {
+					System.out.println("Error con la  conexión de BD");
+				}
+			}
+		});
+		btnNewButton_10.setFont(new Font("Arial Black", Font.BOLD | Font.ITALIC, 14));
+		btnNewButton_10.setBackground(Color.ORANGE);
+		btnNewButton_10.setBounds(527, 404, 137, 34);
+		contentPane.add(btnNewButton_10);
+		
+		JButton btnNewButton_11 = new JButton("REGISTRAR");
+		btnNewButton_11.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Connection con = null;
+				try {	
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://localhost/restaurante";
+					String usr = "root";
+					//String psw = "RARO97";
+					String psw = "";
+					con = DriverManager.getConnection(url, usr, psw);
+					String usuario= textField_10.getText();
+					String query = "INSERT INTO bebida_preparadas (bebida) values ('"+usuario+"')";
+					Statement stm = con.createStatement();
+					stm.executeUpdate(query);
+					textField.setText("");
+					JOptionPane.showMessageDialog(null, "USUARIO INGRESADO A LA BD");
+				} catch (ClassNotFoundException e1) {
+					System.out.println("Error");
+				} catch (SQLException e1) {
+					System.out.println("Error con la  conexión de BD");
+				}
+			}
+		});
+		btnNewButton_11.setFont(new Font("Arial Black", Font.BOLD | Font.ITALIC, 14));
+		btnNewButton_11.setBackground(Color.ORANGE);
+		btnNewButton_11.setBounds(527, 487, 137, 32);
+		contentPane.add(btnNewButton_11);
+		
+		textField_9 = new JTextField();
+		textField_9.setBounds(686, 405, 338, 32);
+		contentPane.add(textField_9);
+		textField_9.setColumns(10);
+		
+		textField_10 = new JTextField();
+		textField_10.setBounds(686, 487, 338, 32);
+		contentPane.add(textField_10);
+		textField_10.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("REFRESCOS");
+		lblNewLabel.setFont(new Font("Arial Black", Font.ITALIC, 14));
+		lblNewLabel.setForeground(Color.ORANGE);
+		lblNewLabel.setBounds(20, 380, 121, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_2 = new JLabel("BEBIDAS CON ALCOHOL");
+		lblNewLabel_2.setFont(new Font("Arial Black", Font.ITALIC, 14));
+		lblNewLabel_2.setForeground(Color.ORANGE);
+		lblNewLabel_2.setBounds(20, 462, 251, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("AGUAS");
+		lblNewLabel_3.setFont(new Font("Arial Black", Font.ITALIC, 14));
+		lblNewLabel_3.setForeground(Color.ORANGE);
+		lblNewLabel_3.setBounds(686, 380, 129, 14);
+		contentPane.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("BEBIDAS PREPARADAS");
+		lblNewLabel_4.setForeground(Color.ORANGE);
+		lblNewLabel_4.setFont(new Font("Arial Black", Font.ITALIC, 14));
+		lblNewLabel_4.setBounds(685, 460, 203, 14);
+		contentPane.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("New label");
+		lblNewLabel_5.setIcon(new ImageIcon(alta.class.getResource("/ima1/alta1.jpg")));
+		lblNewLabel_5.setBounds(0, 0, 1184, 561);
+		contentPane.add(lblNewLabel_5);
 	}
 }
